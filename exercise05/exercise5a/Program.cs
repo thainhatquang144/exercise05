6ab1b274e670174cee31ebd76d1794e66ccc6eef
quang <thaiquang816@gmail.com>
2019-10-08 16:57:03 +0700

    Add exercise5a

---
+++ exercise05/exercise5a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise5a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
